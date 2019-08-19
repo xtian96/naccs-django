c09d7724338d2e3b6729dc6cc4df3816ca712a1f
David Wang <davidw1339@gmail.com>
2019-08-18 20:24:12 -0400

    Faceit Oauth (#3)
    * added rough faceit code
    
    * merge2.0
    
    * pulled out faceit auth into function

--- naccsweb/settings/urls.py
+++ naccsweb/settings/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path(r'account', views.account, name='account'),
+    path(r'account/faceitconfirm', views.faceit, name='faceitconfirm'),
     path(r'account/discordcallback', views.discord, name='discord')
 ]
--- naccsweb/settings/views.py
+++ naccsweb/settings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+import base64
 from django.contrib.auth.decorators import login_required
 import requests
 import os
@@ -8,6 +9,11 @@ DISCORD_ENDPOINT = 'https://discordapp.com/api'
 DISCORD_TOKEN_URL = DISCORD_ENDPOINT + '/oauth2/token'
 DISCORD_CLIENT_ID = os.environ.get('DISCORD_CLIENT_ID')
 DISCORD_CLIENT_SECRET = os.environ.get('DISCORD_CLIENT_SECRET')
+
+FACEIT_TOKEN_ENDPOINT = 'https://api.faceit.com/auth/v1/oauth/token'
+FACEIT_INFO_ENDPOINT = 'https://api.faceit.com/auth/v1/resources/userinfo'
+FACEIT_CLIENT_ID = os.environ.get('FACEIT_CLIENT_ID')
+FACEIT_CLIENT_SECRET = os.environ.get('FACEIT_CLIENT_SECRET')
 
 def get_discord_name(code):
     data = {
@@ -33,10 +39,44 @@ def get_discord_name(code):
     r = requests.get(DISCORD_ENDPOINT + '/users/@me', headers=headers)
     return r.json()['username']
 
+def get_faceit_name(faceit_code):
+    data = {
+        'code': faceit_code,
+        'grant_type': 'authorization_code'
+    }
+
+    authorization = f'{FACEIT_CLIENT_ID}:{FACEIT_CLIENT_SECRET}'.encode()
+    authorization_enc = base64.b64encode(authorization)
+    headers = {
+        'Authorization': 'Basic ' + authorization_enc.decode()
+    }
+
+    # exchange code for authorization token
+    response = requests.post(FACEIT_TOKEN_ENDPOINT, data=data, headers=headers)
+
+    access_token = response.json().get('access_token')
+    headers = {
+        'Authorization': 'Bearer ' + access_token
+    }
+    response =  requests.get(FACEIT_INFO_ENDPOINT, headers=headers)
+    return response.json().get('nickname')
+
 @login_required
 def account(request):
     return render(request, 'settings/account.html')
 
+@login_required
+def faceit(request):
+    faceit_code = request.GET.get('code')
+    faceit_username = get_faceit_name(faceit_code)
+
+    # Enter faceit name into user profile
+    user = User.objects.get(username=request.user.username)
+    user.profile.faceit = faceit_username
+    user.save()
+
+    return redirect('account')
+    
 @login_required
 def discord(request):
     discord_code = request.GET.get('code')
