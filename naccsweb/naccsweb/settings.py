dcc8de3ef2477bf4267d4405e29e536d98634a8e
Eric Lee <ericdl2@illinois.edu>
2019-08-20 02:03:04 -0400

    more fat stuff

--- naccsweb/naccsweb/settings.py
+++ naccsweb/naccsweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.254.25']
+ALLOWED_HOSTS = ['192.168.254.25', 'localhost']
 
 # Application definition
 
