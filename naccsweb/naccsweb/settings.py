5f4ec4e5f8522e000db52cdf1a9ad1f0dcfac31e
Eric Lee <ericdl2@illinois.edu>
2019-08-21 23:40:36 -0400

    Migrate to MySQL

--- naccsweb/naccsweb/settings.py
+++ naccsweb/naccsweb/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'naccsweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_DB'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': 3306
     }
 }
 
